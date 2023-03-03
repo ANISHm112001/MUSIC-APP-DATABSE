2e760548728c9ab3c9c9f81b9c68e9e37523b09d
Anish Mohanty <78677848+ANISHm112001@users.noreply.github.com>
2023-03-03 21:13:48 +0530

    Add files via upload

---
+++ TIIME.sql
@@ -0,0 +1,16 @@
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2018-10-31', '321', '133', '4', '32', '2', '27');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2022-08-30', '288', '189', '7', '43', '5', '31');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2020-06-29', '291', '213', '4', '56', '6', '25');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2016-12-25', '341', '303', '4', '23', '1', '40');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2021-04-16', '200', '121', '4', '18', '3', '18');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2017-11-30', '180', '100', '4', '45', '5', '16');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2019-02-14', '192', '342', '4', '30', '4', '33');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2022-07-30', '344', '145', '4', '19', '3', '29');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2020-01-12', '208', '300', '4', '22', '3', '20');
+INSERT INTO `music_databse`.`tiime` (`START_TIME`, `HOURS`, `DAYS`, `WEEKS`, `MONTHS`, `YEARS`, `WEEKDAYS`) VALUES ('2018-09-21', '348', '78', '4', '40', '1', '21');
+
+
+
+USE MUSIC_DATABSE;
+
+SELECT * FROM TIIME;
---
+++ USERS.sql
@@ -0,0 +1,16 @@
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U01', 'JOHN', 'TIMBER', 'M');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U02', 'LINA', 'PAOLO', 'F');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U03', 'LIONEL', 'MARAZZI', 'M');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U04', 'DAVID', 'HILL', 'T');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U05', 'JON', 'MOXELY', 'M');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U06', 'SAMOA', 'JOE', 'M');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U07', 'CHLOE', 'SILVER', 'T');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U08', 'BARRET', 'WOODLY', 'M');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U09', 'AMANDA', 'NUNES', 'F');
+INSERT INTO `music_databse`.`users` (`USER_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`) VALUES ('U10', 'CECILE', 'FLAIR', 'F');
+
+
+
+USE MUSIC_DATABSE;
+
+SELECT * FROM USERS;
